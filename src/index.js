e63f9b0f0381fb673218629d18a584c23359e39f
Johanna Eriksson <johanna.eriksson@schibsted.com>
2024-09-11 13:06:24 +0200

    Made the box change colors

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
 console.log("Start Here 😉");
+
+const changeBox = document.getElementById("box")
+
+const changeTheme = (theme) => {
+  changeBox.classList.remove("dark", "light", "crazy")
+  changeBox.classList.add(theme)
+}
+
+document.getElementById("dark").addEventListener("click", () => changeTheme("dark"))
+document.getElementById("light").addEventListener("click", () => changeTheme("light"))
+document.getElementById("crazy").addEventListener("click", () => changeTheme("crazy"))
+
+
